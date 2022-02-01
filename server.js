b1e19f50f881982352eb124019dd6dbf402063b6
rawziNael <rawzi.homran@yahoo.com>
2022-02-01 14:47:16 +0200

    task11

--- server.js
+++ server.js
@@ -1,42 +1,43 @@
 "use strict";
+
 const express = require('express');
 const cors = require('cors');
-const data = require('./data.json');
+const info = require('./data.json');
 
 
 const server = express();
 server.use(cors());
 
-server.get('/' , Home);
-server.get('/favorite' , Favorite);
-server.get('*' , InCase1);
-server.get('*' , InCase2);
+server.get('/', Home);
+server.get('/favorite', Favorite);
+server.get('*', InCase1);
+server.get('*', InCase2);
 
-
-
-function Info(data){
+function Info(title , poster_path ,overview) {
     this.title = title;
     this.poster_path = poster_path;
     this.overview = overview;
 }
 
+function Home(request, response) {
+  
+    let obj =  new Info(info.title , info.poster_path , info.overview);
 
-function Home(request , response){
-    let info =  Info(data);
-    response.status(200).json(info);
+    
+    return response.status(200).json(obj);
 }
 
-function Favorite(request , response){
+function Favorite(request, response) {
     response.status(200).send("Welcome to Favorite Page");
 }
 
-function  InCase1(request , response){
+function InCase1(request, response) {
     response.status(404).send("We sorry, you chosed something not exist ");
 }
-function  InCase2(request , response){
+function InCase2(request, response) {
     response.status(500).send("Sorry, something went wrong");
 }
 
-server.listen(3000 , ()=>{
+server.listen(3000, () => {
     console.log("You'r now listening to Rawzi's server");
 })
