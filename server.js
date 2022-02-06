fbb64d1734363764aaee24fcc8b71e0bf4ae2907
RAWZI NAEL <97670198+rawziNael@users.noreply.github.com>
2022-02-06 13:12:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -47,11 +47,6 @@ server.get('*', InCase);
 server.get(errorFix);
 
 
-// const client = new pg.Client(process.env.DATABASE_URL)
-const client = new pg.Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
-})
 
 const client = new pg.Client(process.env.DATABASE_URL);
 
