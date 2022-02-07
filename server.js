922556e99b965c0336be49eda1f489ce564e84ba
rawziNael <rawzi.homran@yahoo.com>
2022-02-07 14:29:42 +0200

    task14

--- server.js
+++ server.js
@@ -7,77 +7,63 @@ const axios = require('axios');
 const pg = require('pg');
 const req = require('express/lib/request');
 const res = require('express/lib/response');
-
-// const info = require('./data.json');
+const movie = require('./data.json');
 
 require('dotenv').config();
 const PORT = process.env.PORT;
-
-
-const info = require('./data.json');
-
-
-
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 
+//Task 11
 server.get('/', Home);
 server.get('/favorite', Favorite);
 
+//Task12
 server.get('/trending', GetTranding);
 server.get('/search', GetSearch);
 
-//Create request to the database
+//Task13
 server.post('/addMovie', addBestMov);
 server.get('/getMovies', getBestMov);
 
-//delete and update methods
+//Task14
 server.put('/update/:id', updateMovie);
 server.delete('/delete/:id', deleteMovie);
-server.get('/getMovie/:id' , getYourMovie)
-
-
+server.get('/getMovie/:id', getYourMovie);
 
 //error handler
-server.get('*', InCase);
+server.get('*', pageError);
 server.get(errorFix);
-
-
-// const client = new pg.Client(process.env.DATABASE_URL)
-const client = new pg.Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
-})
 
 const client = new pg.Client(process.env.DATABASE_URL);
 
 
 function Movie(id, title, release_date, poster_path, overview) {
     this.id = id;
-
-server.get('*', InCase1);
-server.get('*', InCase2);
-
-function Info(title , poster_path ,overview) {
-
     this.title = title;
     this.release_date = release_date;
     this.poster_path = poster_path;
     this.overview = overview;
 }
 
-function Home(request, response) {
-  
-    let obj =  new Info(info.title , info.poster_path , info.overview);
+// function Home(request, response) {
+//     let obj = new Movie(movie.title, movie.poster_path, movie.overview);
+//     response.status(200).send(obj);
+// }
 
- 
+//=========================================Task11===================================================
+
 function Home(request, response) {
     response.status(200).send("This is a home page");
 }
 
-//===============================================task12=====================================================
+function Favorite(request, response) {
+    response.status(200).send("Welcome to Favorite Page");
+}
+
+//=========================================task12=====================================================
 
 function GetTranding(req, res) {
     let url = `https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.APIKEY}&language=en-US`;
@@ -95,10 +81,10 @@ function GetTranding(req, res) {
 
 function GetSearch(req, res) {
     let query = "Eternals";
-    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.APIKEY}&language=en-US&query${query}`;
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.APIKEY}&language=en-US&query=${query}`;
     axios.get(url).then((result) => {
         let search = result.data.results.map(film => {
-            return new Movie(film.title);
+            return new Movie(film.id, film.title, film.release_date, film.poster_path, film.overview);
         })
         res.status(200).send(search);
     }).catch(error => {
@@ -107,13 +93,13 @@ function GetSearch(req, res) {
 }
 
 
-//==================================== New Branch ===============================================
+//==================================== Task13 ===============================================
+
 function addBestMov(req, res) {
     const mov = req.body;
     let sql = `INSERT INTO bestMovie(title,release_date,poster_path,overview) VALUES ($1,$2,$3,$4) RETURNING *;`
-    let valArr= [mov.title , mov.release_date , mov.poster_path, mov.overview];
-    client.query(sql,valArr).then(data =>{
-
+    let valArr = [mov.title, mov.release_date, mov.poster_path, mov.overview];
+    client.query(sql, valArr).then(data => {
         res.status(200).json(data.rows);
     }).catch(error => {
         errorFix(error, req, res)
@@ -130,82 +116,53 @@ function getBestMov(req, res) {
 }
 
 
-//==============================================================================================
-
-
 //======================================Task14==================================================
 
 
-function updateMovie(req , res){
+function updateMovie(req, res) {
     const id = req.params.id;
     const movie = req.body;
     const sql = `UPDATE bestMovie SET title = $1, release_date = $2, poster_path = $3, overview = $4 WHERE id = $5 RETURNING *;`;
-    let movies = [movie.title, movie.release_date, movie.poster_path, movie.overview,id];
-    client.query(sql,movies).then(data =>{
+    let movies = [movie.title, movie.release_date, movie.poster_path, movie.overview, id];
+    client.query(sql, movies).then(data => {
         res.status(200).json(data.rows)
     }).catch(error => {
-        errorFix(error,req,res)
+        errorFix(error, req, res)
     });
 }
 
-function deleteMovie(req , res){
+function deleteMovie(req, res) {
     const id = req.params.id;
-    const sql = `DELETE FROM bestMovie WHERE id=${id};` 
-    client.query(sql).then(()=>{
+    const sql = `DELETE FROM bestMovie WHERE id=${id};`
+    client.query(sql).then(() => {
         res.status(200).json("The movie has been deleted");
-    }).catch(error=>{
-        errorHandler(error,req,res)
+    }).catch(error => {
+        errorHandler(error, req, res)
     });
 }
 
-function getYourMovie(req , res){
+function getYourMovie(req, res) {
     let id = req.params.id
     let sql = `SELECT * FROM bestMovie WHERE id=${id};`;
-    client.query(sql).then(data=>{
-       res.status(200).json(data.rows);
-    }).catch(error=>{
-        errorHandler(error,req,res)
+    client.query(sql).then(data => {
+        res.status(200).json(data.rows);
+    }).catch(error => {
+        errorHandler(error, req, res)
     });
 }
 
-//=================================================================================================
-
-    
-    return response.status(200).json(obj);
-}
-
-
-function Favorite(request, response) {
-    response.status(200).send("Welcome to Favorite Page");
-}
-
+//======================================errors & connect=========================================
 
 function errorFix(error, res, req) {
-    const err = {
-        status: 500,
-        message: error
-    }
-    res.status(500).send(err);
-}
-function InCase(request, response) {
-    response.status(404).send("We sorry, you chosed something not exist ");
+    res.status(500).json(error);
 }
 
-client.connect().then(()=>{
-    server.listen(PORT,()=>{
+function pageError(request, response) {
+    response.status(404).send("page not found error");
+}
+
+client.connect().then(() => {
+    server.listen(PORT, () => {
         console.log(`listining to port ${PORT}`)
     })
-
-
-server.listen(3000, () => {
-
-function InCase1(request, response) {
-    response.status(404).send("We sorry, you chosed something not exist ");
-}
-function InCase2(request, response) {
-    response.status(500).send("Sorry, something went wrong");
-}
-
-server.listen(3000, () => {
-    console.log("You'r now listening to Rawzi's server");
 })
