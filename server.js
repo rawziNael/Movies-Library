a0697a4aee94dee60ee0584b52d5321539afded9
rawziNael <rawzi.homran@yahoo.com>
2022-02-09 12:29:07 +0200

    task12

--- server.js
+++ server.js
@@ -6,12 +6,6 @@ const axios = require('axios');
 const { response } = require('express');
 require('dotenv').config();
 
-// const PORT = process.env.PORT;
-// const info = require('./data.json');
-
-
-
-
 const server = express();
 server.use(cors());
 
@@ -19,10 +13,14 @@ server.get('/', Home);
 server.get('/favorite', Favorite);
 server.get('/trending', GetTranding);
 server.get('/search' , GetSearch);
+//two route
+server.get('/discover_movie' , GetDisMov);
+server.get('/discover_tv' , GetDisTV);
+
 server.get('*', InCase);
 server.get(errorFix);
 
-
+const PORT = process.env.PORT;
 
 function Movie(id, title, release_date, poster_path, overview) {
     this.id = id;
@@ -47,7 +45,7 @@ function GetTranding(req, res) {
         })
         res.status(200).send(trendy);
     }).catch((err) => {
-        console.log(err);
+        errorFix(error, res, req)
     })
 }
 
@@ -56,11 +54,34 @@ function GetSearch(req, res) {
     let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.APIKEY}&language=en-US&query${query}`;
     axios.get(url).then((result) => {
         let search = result.data.results.map(film => {
-            return new Movie(film.title);
+            return new Movie(film.id, film.title, film.release_date, film.poster_path, film.overview);
         })
         res.status(200).send(search);
     }).catch((err) => {
+        errorFix(error, res, req)
+    })
+}
 
+function GetDisMov(req, res) {
+    let url = ` https://api.themoviedb.org/3/discover/movie?api_key=${process.env.APIKEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`;
+    axios.get(url).then((result) => {
+        let discover = result.data.results.map(film => {
+            return new Movie(film.id, film.title, film.release_date, film.poster_path, film.overview);
+        })
+        res.status(200).send(discover);
+    }).catch((err) => {
+        errorFix(error, res, req)
+    })
+}
+function GetDisTV(req, res) {
+    let url = `https://api.themoviedb.org/3/discover/tv?api_key=${process.env.APIKEY}&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0`;
+    axios.get(url).then((result) => {
+        let tv = result.data.results.map(film => {
+            return new Movie(film.id, film.title, film.release_date, film.poster_path, film.overview);
+        })
+        res.status(200).send(tv);
+    }).catch((err) => {
+        errorFix(error, res, req)
     })
 }
 function Favorite(request, response) {
@@ -74,11 +95,12 @@ function errorFix(error, res, req) {
     }
     res.status(500).send(err);
 }
+
 function InCase(request, response) {
     response.status(404).send("We sorry, you chosed something not exist ");
 }
 
 
-server.listen(3000, () => {
-    console.log("You'r now listening to port");
+server.listen(PORT, () => {
+    console.log(`You'r now listening to ${PORT}`);
 })
